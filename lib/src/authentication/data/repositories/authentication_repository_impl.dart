a7c88d355a885fc346cfd880b18e323bed2ea4ef
Beatriz Dadalto <beatrizdadalto@gmail.com>
2026-01-16 19:47:34 -0300

    Data layer and implementation of auth repository

--- lib/src/authentication/data/repositories/authentication_repository_impl.dart
+++ lib/src/authentication/data/repositories/authentication_repository_impl.dart
@@ -1,20 +1,31 @@
 import 'package:flutter_tests_bloc_firebase_tdd_clean_architecture/core/utils/typedef.dart';
+import 'package:flutter_tests_bloc_firebase_tdd_clean_architecture/src/authentication/data/datasources/authentication_remote_data_source.dart';
 import 'package:flutter_tests_bloc_firebase_tdd_clean_architecture/src/authentication/domain/entities/user.dart';
 import 'package:flutter_tests_bloc_firebase_tdd_clean_architecture/src/authentication/domain/repositories/authentication_repository.dart';
 
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  const AuthenticationRepositoryImpl(this._remoteDataSource);
+
+  final AuthenticationRemoteDataSource _remoteDataSource;
+
   @override
   ResultVoid createUser({
     required String createdAt,
     required String name,
     required String avatar,
   }) {
-    
+    /* Test-Driven Development
+    call the remote data source.
+    check if the method returns the proper data.
+    check if when the remoteDataSource throws an exception, we return a failure
+    and if it doens't throw an exception, we return the actual expected data
+    */
+    // TODO implement createUser. after TDD
+    throw UnimplementedError();
   }
 
   @override
   ResultFuture<List<User>> getUsers() {
-
     throw UnimplementedError();
   }
 }
